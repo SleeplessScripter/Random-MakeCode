a1622d8f5f2f5eb559458705d149766314a9aa06
Thomas C <104847775+SleeplessScripter@users.noreply.github.com>
2024-02-22 14:42:47 -0500

    Music and stuff

--- main.ts
+++ main.ts
@@ -3,36 +3,30 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     info.changeLifeBy(2)
 })
 info.onCountdownEnd(function () {
-    game.over(true)
+	
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeScoreBy(1)
 })
+function Level_Control () {
+	
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
-    if (info.countdown() < 3) {
-        Score = 200 * info.highScore()
-        info.setScore(Math.abs(Score))
-    } else {
-        info.changeLifeBy(-1)
-    }
+    info.changeLifeBy(-1)
 })
 let Enemy_Attacks: Sprite = null
 let Coin: Sprite = null
-let Score = 0
-scene.setBackgroundImage(assets.image`Freeway`)
-let Player_Character = sprites.create(assets.image`Mama`, SpriteKind.Player)
+let Level = 2
+scene.setBackgroundImage(assets.image`Matrix effect`)
+let Player_Character = sprites.create(assets.image`Operating System`, SpriteKind.Player)
+Player_Character.setScale(0.5, ScaleAnchor.Middle)
 controller.moveSprite(Player_Character, 50, 50)
 Player_Character.setStayInScreen(true)
-scroller.scrollBackgroundWithSpeed(-50, 0)
+scroller.scrollBackgroundWithSpeed(0, 50)
 info.startCountdown(15)
-animation.runImageAnimation(
-Player_Character,
-assets.animation`Mama Moving`,
-150,
-true
-)
+music.play(music.createSong(hex`0078000408020305001c000f0a006400f4010a000004000000000000000000000000000000000215000c001000021e2918001c0002202724002800021d2a08001c000e050046006603320000040a002d0000006400140001320002010002200004000800021d2a14001800031d242a20002400041920272c2c003000031e222909010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8001c00000001000403050709040005000205070c000d0001063c003d000105`), music.PlaybackMode.LoopingInBackground)
 forever(function () {
     Coin = sprites.createProjectileFromSide(assets.image`Coin`, -90, 0)
     Coin.y = randint(15, 115)
@@ -207,7 +201,13 @@ forever(function () {
     150,
     true
     )
-    if (info.countdown() > 3.5) {
-        pause(2100)
+    if (Level == 1) {
+        pause(1500)
+    } else if (Level == 2) {
+        pause(1000)
+    } else if (Level == 3) {
+        pause(500)
+    } else {
+    	
     }
 })
