28bc7c9029553b55744787d3a31b9e8d830e3827
Thomas C <104847775+SleeplessScripter@users.noreply.github.com>
2024-02-27 14:41:58 -0500

    Shop Devlopment

--- main.ts
+++ main.ts
@@ -1,35 +1,48 @@
+namespace SpriteKind {
+    export const Shop_Item = SpriteKind.create()
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    info.changeScoreBy(-5)
-    info.changeLifeBy(2)
-})
-info.onCountdownEnd(function () {
-	
+    Countd = info.countdown()
+    info.stopCountdown()
+    lif = info.life()
+    Store = 1
+    effects.blizzard.startScreenEffect()
+    pause(1500)
+    scene.setBackgroundImage(assets.image`Store Background`)
+    effects.blizzard.endScreenEffect()
+    Player_Character.setPosition(80, 100)
+    Speed__1 = sprites.create(assets.image`Speed 1`, SpriteKind.Shop_Item)
+    Speed__2 = sprites.create(assets.image`Speed 2`, SpriteKind.Shop_Item)
+    Speed__2.setPosition(61, 55)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeScoreBy(1)
 })
-function Level_Control () {
-	
-}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeLifeBy(-1)
 })
 let Enemy_Attacks: Sprite = null
 let Coin: Sprite = null
-let Level = 2
-scene.setBackgroundImage(assets.image`Matrix effect`)
-let Player_Character = sprites.create(assets.image`Operating System`, SpriteKind.Player)
+let Speed__2: Sprite = null
+let Speed__1: Sprite = null
+let lif = 0
+let Countd = 0
+let Player_Character: Sprite = null
+let Store = 0
+let Level = 1
+Store = 0
+scene.setBackgroundImage(assets.image`Stage`)
+Player_Character = sprites.create(assets.image`Operating System`, SpriteKind.Player)
 Player_Character.setScale(0.5, ScaleAnchor.Middle)
 controller.moveSprite(Player_Character, 50, 50)
 Player_Character.setStayInScreen(true)
-scroller.scrollBackgroundWithSpeed(0, 50)
 info.startCountdown(15)
-music.play(music.createSong(hex`0078000408020305001c000f0a006400f4010a000004000000000000000000000000000000000215000c001000021e2918001c0002202724002800021d2a08001c000e050046006603320000040a002d0000006400140001320002010002200004000800021d2a14001800031d242a20002400041920272c2c003000031e222909010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8001c00000001000403050709040005000205070c000d0001063c003d000105`), music.PlaybackMode.LoopingInBackground)
+music.play(music.createSong(assets.song`Music`), music.PlaybackMode.LoopingInBackground)
 forever(function () {
     Coin = sprites.createProjectileFromSide(assets.image`Coin`, -90, 0)
-    Coin.y = randint(15, 115)
+    Coin.y = randint(40, 96)
     animation.runImageAnimation(
     Coin,
     assets.animation`Animated Coin`,
@@ -37,10 +50,11 @@ forever(function () {
     true
     )
     pause(1000)
+    pauseUntil(() => Store != 1)
 })
 forever(function () {
     Enemy_Attacks = sprites.createProjectileFromSide(assets.image`Worm Virus`, -90, 0)
-    Enemy_Attacks.y = randint(15, 115)
+    Enemy_Attacks.y = randint(40, 96)
     Enemy_Attacks.setKind(SpriteKind.Enemy)
     animation.runImageAnimation(
     Enemy_Attacks,
@@ -207,7 +221,6 @@ forever(function () {
         pause(1000)
     } else if (Level == 3) {
         pause(500)
-    } else {
-    	
     }
+    pauseUntil(() => Store != 1)
 })
