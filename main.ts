04274b7423707756dea8b02172c832d2238d9b15
Thomas C <104847775+SleeplessScripter@users.noreply.github.com>
2024-02-28 14:50:58 -0500

    more store

--- main.ts
+++ main.ts
@@ -12,12 +12,49 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     effects.blizzard.endScreenEffect()
     Player_Character.setPosition(80, 100)
     Speed__1 = sprites.create(assets.image`Speed 1`, SpriteKind.Shop_Item)
+    Speed__1.setPosition(28, 55)
     Speed__2 = sprites.create(assets.image`Speed 2`, SpriteKind.Shop_Item)
-    Speed__2.setPosition(61, 55)
+    Speed__2.setPosition(60, 55)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Shop_Item, function (sprite, otherSprite) {
+    if (otherSprite == Speed__1 && Purchased_S1 == 0) {
+        Purchased_S1 = 1
+        info.changeScoreBy(-10.7)
+        controller.moveSprite(Player_Character, 75, 75)
+        sprites.destroy(Speed__1)
+        Speed__1 = sprites.create(img`
+            222............222
+            2222.....2....2222
+            2222222222....2222
+            .222222221...2222.
+            .222221111..22222.
+            ..22221111..2222..
+            ..1222211112222...
+            ..1222221122222...
+            ..1122221122221...
+            ...112222222211...
+            ....1222222221....
+            .....12222221.....
+            .......2222.......
+            ...7..222222......
+            ..777.2222222.....
+            ...7.22222222.....
+            ....22222.2222....
+            ..7.22227722222...
+            7772222....2222...
+            7.2222277...2222..
+            7.22227.7...22222.
+            72222...7...72222.
+            22222...7...7.2222
+            22227...7...7.2222
+            22277.7777...77222
+            `, SpriteKind.Shop_Item)
+        Speed__1.setPosition(28, 55)
+    }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
@@ -29,6 +66,7 @@ let Speed__2: Sprite = null
 let Speed__1: Sprite = null
 let lif = 0
 let Countd = 0
+let Purchased_S1 = 0
 let Player_Character: Sprite = null
 let Store = 0
 let Level = 1
@@ -38,6 +76,7 @@ Player_Character = sprites.create(assets.image`Operating System`, SpriteKind.Pla
 Player_Character.setScale(0.5, ScaleAnchor.Middle)
 controller.moveSprite(Player_Character, 50, 50)
 Player_Character.setStayInScreen(true)
+Purchased_S1 = 0
 info.startCountdown(15)
 music.play(music.createSong(assets.song`Music`), music.PlaybackMode.LoopingInBackground)
 forever(function () {
